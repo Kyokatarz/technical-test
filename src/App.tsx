2a50471164c88bfa83d89f772e4c9706ab550bd6
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-11-09 01:01:41 +0200

    Remove unneeded css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@mui/material/CssBaseline'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CssBaseline />
+      <div
+        className="App"
+        style={{
+          minWidth: '100vw',
+          minHeight: '100vh',
+        }}
+      ></div>
+    </>
+  )
 }
 
-export default App;
+export default App
