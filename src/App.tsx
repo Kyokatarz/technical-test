e3865008005b3b52bbf0ba01e8fde9156fa1d555
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-11-09 23:33:32 +0200

    Theme and theme provder

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 import React from 'react'
 
 import Dashboard from './components/Dashboard'
+import theme from './theme/theme'
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <div
-        className="App"
-        style={{
-          minWidth: '100vw',
-          minHeight: '100vh',
-        }}
-      >
-        <Dashboard />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div
+          className="App"
+          style={{
+            minWidth: '100vw',
+            minHeight: '100vh',
+          }}
+        >
+          <Dashboard />
+        </div>
+      </ThemeProvider>
     </>
   )
 }
