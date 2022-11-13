34d85b80c04155bb22c6658a6dcc7fd11a992e71
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-11-13 11:43:51 +0200

    Fix weave new data

--- src/components/TestSuiteHeroSection/helpers/weaveNewData.ts
+++ src/components/TestSuiteHeroSection/helpers/weaveNewData.ts
@@ -1,22 +1,25 @@
 import getRandomWithDelta from '../../../helpers/getRandomWithDelta'
 import numberToUiValue from '../../../helpers/numberToUiValue'
 import defaultData from '../../../test.json'
+import DataTypes from '../../../types/data'
 
 export default function weaveNewData() {
-  const newData = { ...defaultData }
-  const oldScore = defaultData.results[0].scores.overallScore.score
-  const oldGpuScore = defaultData.results[0].scores.componentScores[0].score
-  const oldCpuScore = defaultData.results[0].scores.componentScores[1].score
+  const newData: DataTypes = JSON.parse(JSON.stringify({ ...defaultData }))
+  const defaultScore = defaultData.results[0].scores.overallScore.score
+  const defaultGpuScore = defaultData.results[0].scores.componentScores[0].score
+  const defaultCpuScore = defaultData.results[0].scores.componentScores[1].score
 
-  const newScore = getRandomWithDelta(oldScore, 100)
-  const newUiValue = numberToUiValue(newScore)
-  const newGpuScore = getRandomWithDelta(oldGpuScore, 100)
-  const newCpuscore = getRandomWithDelta(oldCpuScore, 100)
+  const newScore = getRandomWithDelta(defaultScore, 100)
+  const newGpuScore = getRandomWithDelta(defaultGpuScore, 100)
+  const newCpuscore = getRandomWithDelta(defaultCpuScore, 100)
 
   newData.results[0].scores.overallScore.score = newScore
-  newData.results[0].scores.overallScore.uiValue = newUiValue
+  newData.results[0].scores.overallScore.uiValue = numberToUiValue(newScore)
   newData.results[0].scores.componentScores[0].score = newGpuScore
   newData.results[0].scores.componentScores[1].score = newCpuscore
-
+  newData.results[0].scores.componentScores[0].uiValue =
+    numberToUiValue(newGpuScore)
+  newData.results[0].scores.componentScores[1].uiValue =
+    numberToUiValue(newCpuscore)
   return newData
 }
